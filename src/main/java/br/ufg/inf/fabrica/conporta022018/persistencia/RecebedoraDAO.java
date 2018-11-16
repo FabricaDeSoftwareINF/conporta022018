27258527397710c5f390f5591dd221f02a1c0f66
Natalia Marufuji Aoki <nataliamarufuji@gmail.com>
2018-11-16 16:59:14 -0200

    Adição das variáveis

--- src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/RecebedoraDAO.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/RecebedoraDAO.java
@@ -2,6 +2,9 @@ package br.ufg.inf.fabrica.conporta022018.persistencia;
 import br.ufg.inf.fabrica.conporta022018.modelo.Recebedora;
 
 public class RecebedoraDAO extends GenericoDAO{
+	
+	private Recebedora recebedora;
+	private ConnectionFactory entityManager;
 
   /**
    * Mtodo utilizado para salvar ou atualizar as informações de uma recebedora.
