26d2cb89b6588edba69e0e61e4f5867dd8794956
Natalia Marufuji Aoki <nataliamarufuji@gmail.com>
2018-11-16 19:56:46 -0200

    Adição da entidade

--- src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/RecebedoraDAO.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/RecebedoraDAO.java
@@ -1,51 +1,26 @@
 package br.ufg.inf.fabrica.conporta022018.persistencia;
 import br.ufg.inf.fabrica.conporta022018.modelo.Recebedora;
 
-public class RecebedoraDAO extends GenericoDAO{
+public class RecebedoraDAO extends GenericoDAO<Recebedora>{
 	
 	private Recebedora recebedora;
 	private ConnectionFactory entityManager;
 
-  /**
-   * Mtodo utilizado para salvar ou atualizar as informações de uma recebedora.
-   * @param recebedora
-   * @return
-   * @throws java.lang.Exception
-   */
-  public Recebedora salvar(Recebedora recebedora) throws Exception {
-    return new Recebedora();
-  }
+	@Override
+	public Recebedora salvar(Recebedora recebedora) {
+	    return new Recebedora();
+	}
 
-  /**
-   * Método que apaga a recebedora do banco de dados.
-   * @param id
-   */
-  public void remover(Long id) {
-  }
+    public void remover(Long id) {}
 
-   /**
-   * Método que busca recebedora do banco de dados.
-   * @param id
-   */
-  public Recebedora buscar(Long id) {
-    return new Recebedora();
-  }
+    public Recebedora buscar(Long id) {
+      return new Recebedora();
+    }
 
-   /**
-   * Método que abre uma transação com banco de dados.
-   */
-  public void abrirTransacao() {
-  }
+    public void abrirTransacao() {}
 
-    /**
-   * Método que commita uma transação com banco de dados.
-   */
-  public void commitarTransacao() {
-  }
+	public void commitarTransacao() {}
 
-   /**
-   * Método que faz rollback de uma transação com banco de dados.
-   */
-  public void rollBackTransacao() {
-  }
+    public void rollBackTransacao() {}
+    
 }
