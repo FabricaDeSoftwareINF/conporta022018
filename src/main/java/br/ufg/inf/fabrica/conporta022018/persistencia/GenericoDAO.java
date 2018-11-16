3a7b7b105fb8aafc475f9814c2a45b578ca1aca7
Iago Bruno <ibruno.om@gmail.com>
2018-11-16 00:18:08 -0200

    Adição de arquivo de persistência, dependências do hibernate
    Inclusão da Classe de Conexão, que retorna a instância do EntityManager e melhorias no GenericoDao.

--- src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/GenericoDAO.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/GenericoDAO.java
@@ -1,4 +1,32 @@
 package br.ufg.inf.fabrica.conporta022018.persistencia;
 
-public class GenericoDAO {
+import javax.persistence.EntityManager;
+
+/**
+ * Classe abstrata de persistência.
+ *
+ * @param <T> modelo que será persistido e cosnultado
+ */
+public abstract class GenericoDAO<T> {
+
+  /**
+   * Salva ou altera um modelo no banco de dados.
+   *
+   * @param modelo que será persistido ou alterado
+   * @return um instância do modelo gerenciado pelo {@link EntityManager}, caso ocorra algum erro,
+   * retornará null.
+   */
+  public T salvar(T modelo) {
+    EntityManager entityManager = ConnectionFactory.getEntityManager();
+
+    try {
+      return entityManager.merge(modelo);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+
+  }
+
+
 }
