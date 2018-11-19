f1f9d71c74d256ce23549dae26d2392eec7790d7
David Matheus <MSSDavid@users.noreply.github.com>
2018-11-18 23:38:05 -0200

    Implementado classe MensagemDAO

--- src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/MensagemDAO.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/MensagemDAO.java
@@ -1,7 +1,8 @@
 ﻿package br.ufg.inf.fabrica.conporta022018.modelo;
 
-public class Mensagem extends MensagemDAO {
-
+public class Mensagem extends GenericoDAO {
+   
+    private Mensagem mensagem;
     
 }
 
