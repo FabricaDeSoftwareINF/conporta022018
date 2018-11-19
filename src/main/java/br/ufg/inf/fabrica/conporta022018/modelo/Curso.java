623eac6428745f554b5620e13664d4fd255a203c
David Matheus <MSSDavid@users.noreply.github.com>
2018-11-18 22:16:56 -0300

    U

---
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/modelo/Curso.java
@@ -0,0 +1,18 @@
+package br.ufg.inf.fabrica.conporta022018.modelo;
+
+ 
+public class Curso {
+    
+    private NivelCurso nivel;
+    
+    
+    public void setNivelCurs(NivelCurso nivel) {
+        this.nivel = nivel;
+    }
+    
+    public String getNivelCur() {
+        return nivel;
+    }
+    
+  
+}
---
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/modelo/Mensagem.java
@@ -0,0 +1,27 @@
+﻿package br.ufg.inf.fabrica.conporta022018.modelo;
+
+public class Mensagem {
+
+    private String titulo;
+    
+    private String descricao;
+
+    public void setTitulo(String titulo){
+      this.titulo = titulo;
+    }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+    
+    public void setDescricao(String descricao){
+      this.descricao = descricao;
+    }
+    
+    public String getDescricao() {
+        return descricao;
+    }
+    
+}
+
+
