13a9436dba58e3082e1e10540560bb766b885d13
Natalia Marufuji Aoki <nataliamarufuji@gmail.com>
2018-11-22 19:59:16 -0200

    Adição das anotações

--- src/main/java/br/ufg/inf/fabrica/conporta022018/modelo/Recebedora.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/modelo/Recebedora.java
@@ -3,11 +3,18 @@ package br.ufg.inf.fabrica.conporta022018.modelo;
 import java.util.Date;
 import java.io.Serializable;
 
+@Entity
+@Table
 public class Recebedora extends ModeloAbstrato {
      
     private static final long serialVersionUID = 97L; 
-     
+    
+    @Temporal(TemporalType.DATE)
+    @Column
     private Date dtCienciaReeb;
+    
+    @JoinColumn
+    @ManyToMany
     private UndAdm unidadeRecebedora;
 
     public void setDtCienciaReeb(Date dtCienciaReeb){
