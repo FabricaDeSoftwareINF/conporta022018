f279cfcfe50ae25d9912d9fdb0eca3c4f9d837c1
Victor <victor.stillo@gmail.com>
2018-11-22 20:22:51 -0200

    Pessoa e PessoaDAO

--- src/main/java/br/ufg/inf/fabrica/conporta022018/modelo/Pessoa.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/modelo/Pessoa.java
@@ -1,4 +1,91 @@
 package br.ufg.inf.fabrica.conporta022018.modelo;
 
-public class Pessoa {
+import java.io.Serializable;
+import java.util.List;
+
+public class Pessoa implements Serializable {
+
+    private String nomePes;
+    private String cpfPes;
+    private String emailPes;
+    private String senhaUsu;
+    private Boolean ehUsuAtivo;
+    private List<Gestao> gestao;
+    private List<Lotacao> servidor;
+    private List<Matricula> discente;
+
+    public String getNomePes() {
+        return nomePes;
+    }
+
+    public void setNomePes(String nomePes) {
+        this.nomePes = nomePes;
+    }
+
+    public String getCpfPes() {
+        return cpfPes;
+    }
+
+    public void setCpfPes(String cpfPes) {
+        this.cpfPes = cpfPes;
+    }
+
+    public String getEmailPes() {
+        return emailPes;
+    }
+
+    public void setEmailPes(String emailPes) {
+        this.emailPes = emailPes;
+    }
+
+    public String getSenhaUsu() {
+        return senhaUsu;
+    }
+
+    public void setSenhaUsu(String senhaUsu) {
+        this.senhaUsu = senhaUsu;
+    }
+
+    public Boolean getEhUsuAtivo() {
+        return ehUsuAtivo;
+    }
+
+    public Boolean validarSenha(String credential) {
+        if(!credential.isEmpty() && credential.length() >= 6)
+            return  true;
+        return false;
+    }
+
+    public void setEhUsuAtivo(Boolean ehUsuAtivo) {
+        this.ehUsuAtivo = ehUsuAtivo;
+    }
+
+    public Boolean isUsuAtivo() {
+        return this.isUsuAtivo();
+    }
+
+    public void setGestao(Gestao gestor) {
+        this.gestao.add(gestor);
+    }
+
+    public List<Gestao> getGestao() {
+        return this.gestao;
+    }
+
+    public void setServidor(Lotacao servidor) {
+        this.servidor.add(servidor);
+    }
+
+    public List<Lotacao> getServidor() {
+        return this.servidor;
+    }
+
+    public void setDiscente(Matricula discente) {
+        this.discente.add(discente);
+    }
+
+    public List<Matricula> getDiscente() {
+        return this.discente;
+    }
+
 }
--- src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/MensagemDAO.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/MensagemDAO.java
@@ -1,5 +1,7 @@
 ﻿package br.ufg.inf.fabrica.conporta022018.modelo;
 
+import br.ufg.inf.fabrica.conporta022018.persistencia.GenericoDAO;
+
 public class Mensagem extends GenericoDAO {
    
     private Mensagem mensagem;
