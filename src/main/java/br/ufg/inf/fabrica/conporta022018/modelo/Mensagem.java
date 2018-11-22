be8aadc32d7cbba3754e25cc793c61a7acd8863e
David Matheus <MSSDavid@users.noreply.github.com>
2018-11-22 21:14:01 -0200

    Correções e Adição de métodos da Classe.

--- src/main/java/br/ufg/inf/fabrica/conporta022018/modelo/Mensagem.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/modelo/Mensagem.java
@@ -4,10 +4,12 @@ package br.ufg.inf.fabrica.conporta022018.modelo;
  * @author David
  * @since 1.0
  */
+@Entity
 public class Mensagem extends ModeloAbstrato {
-
+  
+  private static final long serialVersionUID = 1L;
+  
   private String titulo;
-
   private String descricao;
 
   public String getTitulo() {
@@ -16,5 +18,13 @@ public class Mensagem extends ModeloAbstrato {
 
   public void setTitulo(String titulo) {
     this.titulo = titulo;
+  }
+  
+    public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
   }
 }
