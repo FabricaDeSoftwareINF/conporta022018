ec8712860be00dae7113beb016e0169c245a8c81
Ana Luísa Burjack <analuisaburjack@gmail.com>
2018-11-22 20:43:04 -0200

    Código obsoleto de ControladorDisc foi excluído.

--- src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorDisc.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorDisc.java
@@ -6,34 +6,6 @@
 
 package br.ufg.inf.fabrica.conporta022018.controlador;
 
-
-import br.ufg.inf.fabrica.conporta022018.persistencia.MatriculadoDAO;
-
 public class ControladorDisc {
-
-    MatriculadoDAO matriculadoDAO = new MatriculadoDAO();
-
-    public void incluirDiscente(String nomePes, int cpfPes, String emailPes) {
-
-//        matriculadoDAO.novoMatriculado(nomePes, cpfPes, emailPes);
-
-
-    }
-
-    public void excluirDiscente(int cpfPes) {
-
-    }
-
-    public void alterarDiscente(String nomePes, int cpfPes, String emailPes) {
-
-    }
-
-    public void importarDiscente(String caminhoDoArquivo) {
-
-    }
-
-    public void notificarDiscente(String emailPes) {
-
-    }
 
 }
