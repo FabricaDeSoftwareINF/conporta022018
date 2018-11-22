fb2b63a5fafff8881eb2dba6445c282dbafb784a
Antonio Silva <antoni-s@outlook.com>
2018-11-22 20:47:15 -0200

    Correções da nova implantação.

--- src/main/java/br/ufg/inf/fabrica/conporta022018/modelo/Permissao.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/modelo/Permissao.java
@@ -7,23 +7,12 @@ import javax.persistence.Id;
 import java.io.Serializable;
 
 @Entity
-public class Permissao implements Serializable {
+public class Permissao extends ModeloAbstrato {
 
     private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
 
     private String urlFuncionalidade;
     private String operacao;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getUrlFuncionalidade() {
         return urlFuncionalidade;
