a9730889a7752519b63f47ba7c1abe2e6603f64e
Natalia Marufuji Aoki <nataliamarufuji@gmail.com>
2018-11-23 19:38:23 -0200

    Inicio implementação controlador

--- src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorExclPorta.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorExclPorta.java
@@ -1,9 +1,18 @@
 package br.ufg.inf.fabrica.conporta022018.controlador;
+package br.ufg.inf.fabrica.conporta022018.modelo;
+
 
 /**
  * Created by Alunoinf_2 on 25/10/2018.
  */
-public class ControladorExclPorta {
+public class ControladorExclPort {
+	
+	private portaria;
+	
+	public ControladorExclPort() {
+		this.portaria = new Portaria();
+	}
+	
     public int exclPorta(int i) {
         return 0;
     }
