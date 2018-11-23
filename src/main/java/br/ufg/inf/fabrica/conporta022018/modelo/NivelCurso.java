a7783108bfd9b77c9e125ca678a58976ff2f58b5
Antonio Silva <antoni-s@outlook.com>
2018-11-23 19:32:05 -0200

    Revert "Dev"

--- src/main/java/br/ufg/inf/fabrica/conporta022018/modelo/NivelCurso.java
+++
@@ -1,8 +0,0 @@
-package br.ufg.inf.fabrica.conporta022018.modelo;
-
-/**
- * @author Iago Bruno
- * @since 1.0
- */
-public enum NivelCurso {
-}
--- src/main/java/br/ufg/inf/fabrica/conporta022018/modelo/PortariaStatus.java
+++
@@ -1,5 +0,0 @@
-package br.ufg.inf.fabrica.conporta022018.modelo;
-
-public enum PortariaStatus {
-    Proposta, Ativa, Expirada, Cancelada;
-}
