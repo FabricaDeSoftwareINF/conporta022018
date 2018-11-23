7086ee42b86aef2fef969835f596989eec82f76a
Antonio Silva <antoni-s@outlook.com>
2018-11-23 20:20:53 -0200

    Update PerfilDAO.java

--- src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/PerfilDAO.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/PerfilDAO.java
@@ -3,7 +3,5 @@ package br.ufg.inf.fabrica.conporta022018.persistencia;
 import br.ufg.inf.fabrica.conporta022018.modelo.Perfil;
 
 public class PerfilDAO extends GenericoDAO<Perfil> {
-
-    private Perfil perfil;
-
+    
 }
