b10bd808466032bf4953bc1ace1787cf6099be4d
Keslley L <keslley_14_20@hotmail.com>
2018-11-25 21:27:49 -0200

    Add condicional para verificar se a portaria é válida para o UC EncPorta

--- src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorEncPort.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorEncPort.java
@@ -5,10 +5,20 @@
  */
 
 package br.ufg.inf.fabrica.conporta022018.controlador;
+import br.ufg.inf.fabrica.conporta022018.modelo.Portaria;
+import br.ufg.inf.fabrica.conporta022018.modelo.PortariaStatus;
+import br.ufg.inf.fabrica.conporta022018.persistencia.PortariaDAO;
 
 public class ControladorEncPort {
 
-    public boolean encPortariaCiencia(String IdPortaria){
+    private PortariaDAO portariaDAO = new PortariaDAO();
+
+    public boolean encPortariaCiencia(Portaria portaria){
+        if ( portaria.getUnidadeExpedidora() == null && (portaria.getStatus() != PortariaStatus.ATIVA || portaria.getStatus() != PortariaStatus.CANCELADA) ){
+            throw new UnsupportedOperationException("Portaria em questão não é válida.");
+        }
+
         return true;
-    }
+
+    ss}
 }
