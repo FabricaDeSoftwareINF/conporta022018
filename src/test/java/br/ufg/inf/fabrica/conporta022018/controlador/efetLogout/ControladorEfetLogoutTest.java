4f67378361bfad06cf393009bb5f285d583269ad
Erick Vieira <erick-vieira-s@outlook.com>
2018-12-06 17:09:25 +0000

    criação do caso de teste provisório para o logout

--- src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/efetLogout/ControladorEfetLogoutTest.java
+++ src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/efetLogout/ControladorEfetLogoutTest.java
@@ -13,122 +13,45 @@ import br.ufg.inf.fabrica.conporta022018.util.LerArquivo;
 import br.ufg.inf.fabrica.conporta022018.util.csv.ExtratorCSV;
 import br.ufg.inf.fabrica.conporta022018.util.exception.EfetLogoutException;
 import org.junit.*;
+import org.mockito.Mockito;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ControladorEfetLogoutTest {
+public class ControladorEfetLogoutTest extends Mockito {
 
     private static ControladorEfetLogout controladorEfetLogout;
 
-    /*
-     * Preparação do ambiente para teste.
-     * População do banco de Dados para atendam os pré-requisitos do caso de uso.
-     */
-
-    @BeforeClass
-    public static void casoTestPepararCenario() throws IOException {
-
-        String CAMINHO_CSV = "src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/efetLogout/EfetLogoutTest.csv";
-        String REGRA = ";";
-        List<String> dadosSoftware = new ArrayList<>();
-        Extrator extrator = new ExtratorCSV();
-        LerArquivo lerArquivo = new LerArquivo();
-        String tabelaAtual = " ";
-        String dadosPessoas[];
-        String dadosSessoes[];
-        String linha;
-        // Inicializa o array de tabelas conhecidas
-        final ArrayList TABELAS_CONHECIDAS = (ArrayList)(Arrays.asList(TabelasConhecidasEnum.values()));
-
-        //Criar as instâncias de todos os objetos DAO's necessários para preparar o cenario.
-        dadosSoftware = lerArquivo.lerArquivo(CAMINHO_CSV);
-
-        for (String dado : dadosSoftware) {
-            linha = dado;
-
-            //Definir as tabelas que serão populadas no Banco de Dados.
-            if (TABELAS_CONHECIDAS.contains(linha)) {
-                tabelaAtual = linha;
-                continue;
-            }
-
-            switch (tabelaAtual) {
-                case "pessoa" :
-                    extrator.setTexto(linha);
-                    dadosPessoas = extrator.getResultado(REGRA);
-                    //Aqui colocar os comandos para popular a tabela pessoa no Banco de Dados.
-                    break;
-                case "sessao" :
-                    extrator.setTexto(linha);
-                    dadosSessoes = extrator.getResultado(REGRA);
-                    //Aqui colocar os comandos para popular a tabela sessao no Banco de Dados.
-                    break;
-            }
-        }
-    }
-
     @Before
     public void casoTestPrepararExecucao() {
-
         //Neste Grupo ficará tudo que é necessário para a execução dos cenarios definidos para os testes.
         controladorEfetLogout = new ControladorEfetLogout();
     }
 
     @Test
-    public void casoTestDadosValidos() throws IOException, EfetLogoutException, ServletException {
+    public void testServlet() throws Exception {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
 
-        //Grupo de teste DadosValidos:
-        String retorno = controladorEfetLogout.efetuarLogout();
-        if (!retorno.equals(RetornoEfetLogoutEnum.OK.getRetorno())) {
-            throw new EfetLogoutException(retorno);
-        }
-        // testa se a sessao foi realmente finalizada
+        when(request.getParameter("username")).thenReturn("me");
+        when(request.getParameter("password")).thenReturn("secret");
 
-    }
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(writer);
 
-    @Test
-    public void casoTestDadosExcecoes() throws IOException, EfetLogoutException, ServletException {
+        controladorEfetLogout.efetuarLogout();
 
-        //Grupo de teste DadosExcecoes:
-        String retorno = controladorEfetLogout.efetuarLogout();
-        if (retorno.equals(RetornoEfetLogoutEnum.ERRO_EXECUCAO.getRetorno())) {
-            throw new EfetLogoutException(retorno);
-        } else if (retorno.equals(RetornoEfetLogoutEnum.ERRO_JA_ENCERRADA.getRetorno())) {
-            throw new EfetLogoutException(retorno);
-        } else if (retorno.equals(RetornoEfetLogoutEnum.ERRO_CONEXAO_BD.getRetorno())) {
-            throw new EfetLogoutException(retorno);
-        } else if (!retorno.equals(RetornoEfetLogoutEnum.OK.getRetorno())) {
-            throw new EfetLogoutException(RetornoEfetLogoutEnum.ERRO_INESPERADO.getRetorno());
-        }
-        //O cenario acima testa a primeira exceção do caso de uso em que a sessao nao pode ser finalizada.
-    }
-
-    @AfterClass
-    public static void casoTestResultados() throws IOException {
-
-        //Nada a fazer neste caso
-    }
-
-    // enum criado para tornar o codigo mais limpo, concentrando os nomes das tabelas
-    private enum TabelasConhecidasEnum {
-
-        SESSAO("sessao"),
-        PESSOA("pessoa");
-
-        private final String tabela;
-
-        TabelasConhecidasEnum(final String tabela) {
-            this.tabela = tabela;
-        }
-
-        public String getTabela() {
-            return tabela;
-        }
-
+        verify(request, atLeast(1)).getParameter("username");
+        writer.flush();
+        Assert.assertTrue(stringWriter.toString().contains("My expected string"));
     }
 
 }
