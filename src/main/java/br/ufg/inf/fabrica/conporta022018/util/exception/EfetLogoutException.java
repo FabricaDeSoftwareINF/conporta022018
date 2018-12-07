67287611c4ccc351787e3d7cfa73f44f14c3ed97
Erick Vieira <erick-vieira-s@outlook.com>
2018-12-07 19:50:49 +0000

    deleção de exception personalizada obsoleta para a atual implementação

--- src/main/java/br/ufg/inf/fabrica/conporta022018/util/exception/EfetLogoutException.java
+++
@@ -1,9 +0,0 @@
-package br.ufg.inf.fabrica.conporta022018.util.exception;
-
-public class EfetLogoutException extends Exception {
-
-    public EfetLogoutException(String message) {
-        super(message);
-    }
-
-}
