28acd010377ce42237a9914a8903dc860ea24587
João Vitor Rodrigues Santillo <jvsantillo@gmail.com>
2018-12-07 12:43:25 -0200

    Adicionando DAOs atualizados

--- src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/PortariaDAO.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/PortariaDAO.java
@@ -3,5 +3,5 @@ package br.ufg.inf.fabrica.conporta022018.persistencia;
 import br.ufg.inf.fabrica.conporta022018.modelo.Portaria;
 
 public class PortariaDAO extends GenericoDAO<Portaria> {
-
+    
 }
--- src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/PortariaReferenciadaDAO.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/PortariaReferenciadaDAO.java
@@ -2,6 +2,6 @@ package br.ufg.inf.fabrica.conporta022018.persistencia;
 
 import br.ufg.inf.fabrica.conporta022018.modelo.Referencia;
 
-public class PortariaReferenciadaDAO extends GenericoDAO<Referencia> {
+public class PortariaReferenciadaDAO extends GenericoDAO<Referencia>{
 
 }
