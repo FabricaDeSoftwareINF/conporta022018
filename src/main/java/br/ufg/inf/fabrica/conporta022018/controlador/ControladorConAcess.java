b14c9300b0326a6256a173c0ad23e26bf1aba1df
antoni-s <antoni-s@outlook.com>
2018-12-09 01:36:53 -0200

    Correção dos testes.

--- src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorConAcess.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorConAcess.java
@@ -13,17 +13,12 @@ import java.util.*;
 
 public class ControladorConAcess {
 
-    private final String JPQL_BUSCAR_USUARIO_POR_CPF = "select u Pessoa u where u.CPF = :cpfPes";
-    private final String JPQL_BUSCAR_PERFIL = "select u Perfil u where u.TIPO_PERFIL = :nome";
+    private final String JPQL_BUSCAR_USUARIO_POR_CPF = "SELECT u FROM Pessoa u WHERE u.cpfPes = :CPF";
+    private final String JPQL_BUSCAR_PERFIL = "SELECT u FROM Perfil u WHERE u.nome = :nome";
     private Map<String, Object> map;
 
     public ControladorConAcess() {
         this.map = new HashMap<String, Object>();
-    }
-
-    public boolean checarLogin(String CPF, String senha) {
-
-        return true;
     }
 
     public Pessoa buscarPorCPF(String CPF) {
@@ -31,68 +26,10 @@ public class ControladorConAcess {
         Pessoa usuario;
         PessoaDAO dao = new PessoaDAO();
 
-        map.put("cpfPes", CPF);
+        map.put("CPF", CPF);
         usuario = dao.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_USUARIO_POR_CPF, map);
 
         return usuario;
-    }
-
-    public List<Permissao> buscarPermissao(Pessoa usuario) {
-
-        boolean moderado = false;
-        boolean restrito = false;
-        boolean superRestrito = false;
-        List<Permissao> permissoes = new ArrayList();
-        PerfilDAO dao = new PerfilDAO();
-
-        for (Matricula matricula : usuario.getDiscente()) {
-            Date data = matricula.getDtFimMatrCur();
-
-            if (data == null)
-                moderado = true;
-        }
-
-        for (Lotacao lotacao : usuario.getServidor()) {
-            Date data = lotacao.getDtFimLotServ();
-            if (data == null)
-                moderado = true;
-        } 
-    
-        for (Gestao gestao : usuario.getGestao()) {
-            if (gestao.getDtFimSubChefe() == null) {
-                if (gestao.getTipo() == Tipo.COORDENADOR_ADM)
-                    restrito = true;
-                if (gestao.getTipo() == Tipo.CHEFIA || gestao.getTipo() == Tipo.SUBSTITUTO) {
-                    superRestrito = true;
-                    restrito = true;
-                }
-            }
-        }
-
-        List<Permissao> temporario = new ArrayList();
-        if (moderado) {
-            map.put("nome","ROLE_MODERADO");
-            temporario = dao.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_PERFIL, map).getPermissoes();
-            for (Permissao permissao : temporario)
-                permissoes.add(permissao);
-            map.remove("ROLE_MODERADO");
-        }
-        if (restrito) {
-            map.put("nome","ROLE_RESTRITO");
-            temporario = dao.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_PERFIL, map).getPermissoes();
-            for (Permissao permissao : temporario)
-                permissoes.add(permissao);
-            map.remove("ROLE_RESTRITO");
-        }
-        if (superRestrito) {
-            map.put("nome","ROLE_RESTRITO_ESPECIFICO");
-            temporario = dao.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_PERFIL, map).getPermissoes();
-            for (Permissao permissao : temporario)
-                permissoes.add(permissao);
-            map.remove("ROLE_RESTRITO_ESPECIFICO");
-        }
-
-        return permissoes;
     }
 
     public List<Perfil> buscarPerfil(Pessoa usuario) {
@@ -100,32 +37,38 @@ public class ControladorConAcess {
         List<Perfil> perfilList = new ArrayList<>();
         PerfilDAO dao = new PerfilDAO();
 
-        for (Matricula matricula : usuario.getDiscente()) {
-            Date data = matricula.getDtFimMatrCur();
+        if (!usuario.getDiscente().equals(null)) {
+            for (Matricula matricula : usuario.getDiscente()) {
+                Date data = matricula.getDtFimMatrCur();
 
-            if (data == null) {
-                map.put("nome","ROLE_MODERADO");
-                perfilList.add(dao.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_PERFIL, map));
-            }
-        }
-
-        for (Lotacao lotacao : usuario.getServidor()) {
-            Date data = lotacao.getDtFimLotServ();
-            if (data == null) {
-                map.put("nome","ROLE_MODERADO");
-                perfilList.add(dao.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_PERFIL, map));
-            }
-        }
-
-        for (Gestao gestao : usuario.getGestao()) {
-            if (gestao.getDtFimSubChefe() == null) {
-                if (gestao.getTipo() == Tipo.COORDENADOR_ADM) {
-                    map.put("nome","ROLE_RESTRITO");
+                if (data == null) {
+                    map.put("nome", "ROLE_MODERADO");
                     perfilList.add(dao.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_PERFIL, map));
                 }
-                if (gestao.getTipo() == Tipo.CHEFIA || gestao.getTipo() == Tipo.SUBSTITUTO) {
-                    map.put("nome","ROLE_RESTRITO_ESPECIFICO");
+            }
+        }
+
+        if (!usuario.getServidor().equals(null)) {
+            for (Lotacao lotacao : usuario.getServidor()) {
+                Date data = lotacao.getDtFimLotServ();
+                if (data == null) {
+                    map.put("nome", "ROLE_MODERADO");
                     perfilList.add(dao.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_PERFIL, map));
+                }
+            }
+        }
+
+        if (!usuario.getGestao().equals(null)) {
+            for (Gestao gestao : usuario.getGestao()) {
+                if (gestao.getDtFimSubChefe() == null) {
+                    if (gestao.getTipo() == Tipo.COORDENADOR_ADM) {
+                        map.put("nome", "ROLE_RESTRITO");
+                        perfilList.add(dao.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_PERFIL, map));
+                    }
+                    if (gestao.getTipo() == Tipo.CHEFIA || gestao.getTipo() == Tipo.SUBSTITUTO) {
+                        map.put("nome", "ROLE_RESTRITO_ESPECIFICO");
+                        perfilList.add(dao.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_PERFIL, map));
+                    }
                 }
             }
         }
--- src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/conAcess/ControladorConAcessTest.java
+++ src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/conAcess/ControladorConAcessTest.java
@@ -4,17 +4,17 @@
  * Creative Commons Attribution 4.0 International License.
  */
 package br.ufg.inf.fabrica.conporta022018.controlador.conAcess;
-
 import br.ufg.inf.fabrica.conporta022018.controlador.ControladorConAcess;
+
 import br.ufg.inf.fabrica.conporta022018.modelo.*;
-import br.ufg.inf.fabrica.conporta022018.persistencia.*;
+import br.ufg.inf.fabrica.conporta022018.persistencia.PerfilDAO;
+import br.ufg.inf.fabrica.conporta022018.persistencia.PessoaDAO;
 import br.ufg.inf.fabrica.conporta022018.util.Extrator;
 import br.ufg.inf.fabrica.conporta022018.util.LerArquivo;
 import br.ufg.inf.fabrica.conporta022018.util.csv.ExtratorCSV;
 import org.junit.*;
 import org.mockito.Mockito;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -26,36 +26,42 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+/*
+ * Classe tem o intuito de exercitar o controlador referente aos casos de uso:
+ *   1. ConAcess;
+ *   2. RefLogin;
+ *   3. IdSoft.
+ * Devido a uma particularidade dos casos de uso acima essa classe de teste será
+ * dividaida em duas partes, sendo elas:
+ *   1ª parte testa o controlador;
+ *   2ª parte testa chamadas ao Framework.
+ * Para os testes a seguinte estrategia foi definida:
+ *   1 - será testado a acurácia dos retornos;
+ *   2 - somente o cenário que se trata de Login será definido:
+ *       2.1 - testes com valores válidos;
+ *       2.2 - testes com valores inválidos.
+ *
+ * A estrategia acima descrita justifica pelo motivo dos casos de uso em sua maioria
+ * se tratar de chamadas internas ao softwares.
+ */
 public class ControladorConAcessTest extends Mockito {
 
-    /*
-    * PARA O CENÁRIO DE CONACESS E IDSOFTWARE SERÁ REALIZADO TESTES NOS MÉTODOS DO CONTROLADOR
-    * QUE O FRAMEWORK USA.
-    *
-    *
-    * Para o cenário de login e controle de acesso será feito os testes atraves de mocki,
-    * pois não possui um servlet dessa forma não possui escuta da url para obter o retorno.
-    *
-    * Os testes aqui realizado apresenta o conceito para os casos de uso
-    *     - ConAcess
-    *     - IdSoft
-     */
     private static ControladorConAcess controlador;
 
+    @Test
+    public void casoTestControladorConAcess() throws IOException, ParseException {
 
-    /*
-     * Preparação do ambiente para teste.
-     * População do banco de Dados para atendam os pré-requisitos do caso de uso.
-     */
-    @BeforeClass
-    public static void casoTestPepararCenario() throws IOException, ParseException {
-
+        /*
+         * Os comandos abaixo prepara o ambiente para a execução dos testes, ou seja,
+         * ele popula a base da dados e realiza configuração de variáveis globais.
+         */
         String CAMINHO_CSV = "ControladorConAcess.csv";
         String REGRA = ",";
         List<String> dadosSoftware = new ArrayList<>();
         Extrator extrator = new ExtratorCSV();
         LerArquivo lerArquivo = new LerArquivo();
         String tabelaAtual = " ";
+        Date defaultDate = new Date(99/99/9999);
         String dados[];
         String linha;
         Pessoa usuario;
@@ -86,7 +92,7 @@ public class ControladorConAcessTest extends Mockito {
                     matricula = new Matricula();
                     curso = new Curso();
                     undAdm = new UndAdm();
-                    SimpleDateFormat formato = new SimpleDateFormat( "dd/MM/yyyy");
+                    SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
                     extrator.setTexto(linha);
                     dados = extrator.getResultado(REGRA);
 
@@ -95,13 +101,13 @@ public class ControladorConAcessTest extends Mockito {
                     usuario.setEmailPes(dados[2]);
                     usuario.setSenhaUsu(dados[3]);
                     usuario.setEhUsuAtivo(Boolean.parseBoolean(dados[4]));
-                    if (dados[5].equals(null)) {
+                    if (dados[5].equals("null")) {
                         usuario.setGestao(gestao);
                     } else {
                         gestao.setdtInicio(formato.parse(dados[5]));
-                        gestao.setdtFim(dados[6].equals(null) ? null : formato.parse(dados[6]));
-                        gestao.setDtIniSubChefe(dados[7].equals(null) ? null : formato.parse(dados[7]));
-                        gestao.setDtFimSubChefe(dados[8].equals(null) ? null : formato.parse(dados[8]));
+                        gestao.setdtFim(formato.parse(dados[6]));
+                        gestao.setDtIniSubChefe(formato.parse(dados[7]));
+                        gestao.setDtFimSubChefe(formato.parse(dados[8]));
 
                         undAdm.setNomeUnd(dados[9]);
                         undAdm.setSiglaUnAdm(dados[10]);
@@ -133,11 +139,11 @@ public class ControladorConAcessTest extends Mockito {
                         usuario.setGestao(gestao);
                     }
                     lotacao = new Lotacao();
-                    if (dados[18].equals(null)) {
+                    if (dados[18].equals("null")) {
                         usuario.setServidor(lotacao);
                     } else {
                         lotacao.setDtIniLotServ(formato.parse(dados[18]));
-                        lotacao.setDtFimLotServ(dados[19].equals(null) ? null : formato.parse(dados[19]));
+                        lotacao.setDtFimLotServ(formato.parse(dados[19]));
                         lotacao.setDescrCargoServ(dados[20]);
                         lotacao.setCargoServ(Cargo.valueOf(dados[21]));
 
@@ -154,7 +160,7 @@ public class ControladorConAcessTest extends Mockito {
                         lotacao.setUndAdm(undAdm);
                         usuario.setServidor(lotacao);
                     }
-                    if (dados[30].equals(null)) {
+                    if (dados[30].equals("null")) {
                         usuario.setDiscente(matricula);
                     } else {
 
@@ -166,77 +172,216 @@ public class ControladorConAcessTest extends Mockito {
                     } catch (Exception exc) {
                         pessoaDAO.rollBackTransacao();
                     }
-                break;
+                    break;
+                case "perfil":
+                    Perfil perfilDB = new Perfil();
+                    List<Permissao> permissaoList = new ArrayList<>();
+                    Permissao permissao = new Permissao();
+                    PerfilDAO perfilDAO = new PerfilDAO();
+                    dados = extrator.getResultado(REGRA);
+
+                    permissao.setUrlFuncionalidade(dados[0]);
+                    permissao.setOperacao(dados[1]);
+                    permissaoList.add(permissao);
+
+                    perfilDB.setPermissoes(permissaoList);
+                    perfilDB.setNome(dados[2]);
+                    perfilDB.setDescricao(dados[3]);
+
+                    perfilDAO.abrirTransacao();
+                    try {
+                        perfilDAO.salvar(perfilDB);
+                        perfilDAO.commitarTransacao();
+                    } catch (Exception exc) {
+                        perfilDAO.rollBackTransacao();
+                    }
+                    break;
             }
         }
+
+        /*            -------->>> FINALIZAÇÂO DA PREPARAÇÃO DO AMBIENTE <<<---------                 */
+
+
+
+
+        /*             -------->>> 1ª Parte - Testes do Controlador  <<<---------                   */
+
+
+
+        /*
+         * Como os casos de uso consomem dados da base de dados os testes estão organizados de
+         * acordo com a estrutura abaixo:
+         *
+         * Seção 1 - Exercita os cenários de sucesso do caso de uso e valida os retornos;
+         * Seção 2 - Exercita os cenários de excerções do caso de uso e valida os retornos.
+         */
+
+
+
+        /*                -------->>> SEÇÃO 1 - CENÁRIOS DE SUCESSO  <<<---------                    */
+
+        controlador = new ControladorConAcess();
+
+        /*
+         * Cenário testa chamada da busca por cpf com cpf válido.
+         *
+         * Espera-se "True" como retorno da válidação dos dados tragos da base de dados.
+         */
+        usuario = controlador.buscarPorCPF("785.123.543-78");
+        Assert.assertEquals("785.123.543-78", usuario.getCpfPes());
+        Assert.assertEquals("José Carlos de Souza", usuario.getNomePes());
+        Assert.assertEquals("jose@exemplo.com", usuario.getEmailPes());
+
+        usuario = controlador.buscarPorCPF("342.897.456-34");
+        Assert.assertEquals("342.897.456-34", usuario.getCpfPes());
+        Assert.assertEquals("Fermanda Maria da Silva", usuario.getNomePes());
+        Assert.assertEquals("fernanda@exemple.com", usuario.getEmailPes());
+
+        usuario = controlador.buscarPorCPF("764.098.612-09");
+        Assert.assertEquals("764.098.612-09", usuario.getCpfPes());
+        Assert.assertEquals("Pedro da Silva", usuario.getNomePes());
+        Assert.assertEquals("pedro@exemple.com", usuario.getEmailPes());
+
+        usuario = controlador.buscarPorCPF("456.095.124-98");
+        Assert.assertEquals("456.095.124-98", usuario.getCpfPes());
+        Assert.assertEquals("Maria José de Oliveira", usuario.getNomePes());
+        Assert.assertEquals("maria@exemple.com", usuario.getEmailPes());
+
+
+        /*
+         * Cenário testa chamada a busca de perfil.
+         *
+         * Espera-se "True" como retorno da válidação dos dados tragos da base de dados.
+         */
+        List<Perfil> perfilList = new ArrayList<>();
+        List<Perfil> retornoBaseDados;
+
+        //Teste de busca por perfil moderado.
+        Perfil perfilModerado = new Perfil();
+        perfilModerado.setNome("ROLE_MODERADO");
+        perfilModerado.setDescricao("Perfil de Designados.");
+        List<Permissao> permissaoList = new ArrayList<>();
+        Permissao permissaoModerado = new Permissao();
+        permissaoModerado.setUrlFuncionalidade("/Moderado");
+        permissaoModerado.setOperacao("Ciencia");
+        permissaoList.add(permissaoModerado);
+        perfilModerado.setPermissoes(permissaoList);
+        perfilList.add(perfilModerado);
+
+        usuario = controlador.buscarPorCPF("777.876.908-98");
+        retornoBaseDados = controlador.buscarPerfil(usuario);
+        Assert.assertEquals(perfilList, retornoBaseDados);
+        Assert.assertEquals(1, perfilList.size());
+        Assert.assertEquals("ROLE_MODERADO", retornoBaseDados.get(0).getNome());
+        Assert.assertEquals("/Moderado", retornoBaseDados.get(0).getPermissoes().get(0).getUrlFuncionalidade());
+
+        //Teste busca por perfil restrito.
+        Perfil perfilRestrito = new Perfil();
+        perfilRestrito.setNome("ROLE_RESTRITO");
+        perfilRestrito.setDescricao("Perfil de Coordenador Administrativo");
+        List<Permissao> permissaoRestrito = new ArrayList<>();
+        Permissao permissaoRest = new Permissao();
+        permissaoRest.setUrlFuncionalidade("/Restrito");
+        permissaoRest.setOperacao("Relátorios");
+        permissaoRestrito.add(permissaoRest);
+        perfilRestrito.setPermissoes(permissaoRestrito);
+        perfilList.add(perfilRestrito);
+
+        usuario = controlador.buscarPorCPF("456.987.231-99");
+        retornoBaseDados = controlador.buscarPerfil(usuario);
+        Assert.assertEquals(perfilList, retornoBaseDados);
+        Assert.assertEquals(2, perfilList.size());
+        Assert.assertEquals("ROLE_RESTRITO", retornoBaseDados.get(1).getNome());
+        Assert.assertEquals("/Restrito", retornoBaseDados.get(1).getPermissoes().get(0).getUrlFuncionalidade());
+
+        //Teste busca por perfil restrito especifico.
+        Perfil perfilEspecifico = new Perfil();
+        perfilEspecifico.setNome("ROLE_RESTRITO_ESPECIFICO");
+        perfilEspecifico.setDescricao("Chefe e Substituto de Chefe");
+        List<Permissao> permissaoEspecifico = new ArrayList<>();
+        Permissao permissaoEsp = new Permissao();
+        permissaoEsp.setUrlFuncionalidade("/Especifico");
+        permissaoEsp.setOperacao("Todas");
+        permissaoEspecifico.add(permissaoEsp);
+        perfilEspecifico.setPermissoes(permissaoEspecifico);
+        perfilList.add(perfilEspecifico);
+
+        usuario = controlador.buscarPorCPF("345.185.165-97");
+        retornoBaseDados = controlador.buscarPerfil(usuario);
+        Assert.assertEquals(perfilList, retornoBaseDados);
+        Assert.assertEquals(3, perfilList.size());
+        Assert.assertEquals("ROLE_RESTRITO_ESPECIFICO", retornoBaseDados.get(2).getNome());
+        Assert.assertEquals("/Especifico", retornoBaseDados.get(2).getPermissoes().get(0).getUrlFuncionalidade());
+
+
+
+        /*                -------->>> SEÇÃO 2 - CENÁRIOS DE EXCERÇÕES   <<<---------                  */
+
+        /*
+         * Cenário que testa o controlador quando o usuário não existe na base de dados.
+         *
+         * Para não parar o codigo será tratado as excerções no try/catch.
+         *
+         * Espera-se o usuário como null na válidação da saída.
+         */
+        usuario = null;
+        try {
+            usuario = controlador.buscarPorCPF("000.000.000-00");
+        } catch (Exception e) {
+            //Tratamento apenas para o caso de teste não para.
+        }
+        Assert.assertEquals(null, usuario);
+
+        try {
+            usuario = controlador.buscarPorCPF("999.999.999-99");
+        } catch (Exception e) {
+            //Tratamento apenas para o caso de teste não para.
+        }
+        Assert.assertEquals(null, usuario);
+
+        try {
+            usuario = controlador.buscarPorCPF("888.888.888-88");
+        } catch (Exception e) {
+            //Tratamento apenas para o caso de teste não para.
+        }
+        Assert.assertEquals(null, usuario);
+
+        try {
+            usuario = controlador.buscarPorCPF("777.777.777-77");
+        } catch (Exception e) {
+            //Tratamento apenas para o caso de teste não para.
+        }
+        Assert.assertEquals(null, usuario);
+
+
+
+
+        /*             -------->>> 2ª Parte - Testes do Framework  <<<---------                   */
+
+
+        /*
+         * Esta parte exercita o Framework.
+         *
+         * Por limitação os teste apresentados abaixo são mocki, pois não possui um servelet
+         * para fazer chamadas, assim não ah ninguém escultando as chamadas que seria realizada
+         * pelos teste, sendo impossível testa. Porém como o Framework consomem os dados do
+         * controlador testado acima assume-se como verdade os testes abaixo.
+         */
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+
+        when(request.getParameter("username")).thenReturn("me");
+        when(request.getParameter("password")).thenReturn("secret");
+
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(writer);
+
+        verify(request, atLeast(1)).getParameter("username");
+        writer.flush();
+        Assert.assertTrue(stringWriter.toString().contains("My expected string"));
+
     }
 
-    @Test
-    public void casoTestDadosValidos() throws Exception {
-
-        Pessoa usuario;
-
-        //Testa um usuário que existe no banco de dados.
-        usuario = controlador.buscarPorCPF("123.123.123-12");
-
-        //Verifica se o usuário é o mesmo que está atrelado a esse cpf na base de dados.
-        Assert.assertEquals("José Paulo Souza", usuario.getNomePes());
-        Assert.assertEquals("123.123.123-12", usuario.getCpfPes());
-        Assert.assertEquals("jose@example.com", usuario.getEmailPes());
-
-        //Testa outro usuário que existe na base de dados.
-        usuario = controlador.buscarPorCPF("789.789.789-89");
-
-        //Verifica se o usuário é o mesmo que está atrelado a esse cpf na base de dados.
-        Assert.assertEquals("Fernanda Paula de Souza", usuario.getNomePes());
-        Assert.assertEquals("789.789.789-89", usuario.getCpfPes());
-        Assert.assertEquals("fernanda@example.com", usuario.getEmailPes());
-
-    }
-
-    @Test
-    public void casoTestDadosExcecoes() throws Exception {
-
-        Pessoa usuario;
-
-        //Testa usuário que não existe na base de dados.
-        usuario = controlador.buscarPorCPF("789.789.789-12");
-
-        //Os dados deve retornar null.
-        Assert.assertEquals(null, usuario.getNomePes());
-        Assert.assertEquals(null, usuario.getCpfPes());
-        Assert.assertEquals(null, usuario.getEmailPes());
-
-        //Testa outro usuário que não existe na base de dados.
-        usuario = controlador.buscarPorCPF("789.789.789-56");
-
-        //Os dados deve retornar null.
-        Assert.assertEquals(null, usuario.getNomePes());
-        Assert.assertEquals(null, usuario.getCpfPes());
-        Assert.assertEquals(null, usuario.getEmailPes());
-
-        //Como não existe usuário espera se que venha null as permissões.
-        Assert.assertEquals(true, controlador.buscarPermissao(usuario).equals(null));
-
-        //Como não existe usuário espera se que venha null a lista de perfil.
-        Assert.assertEquals(true, controlador.buscarPerfil(usuario).equals(null));
-
-    }
-
-//    @Test
-//    public void TestControlador() throws Exception {
-//        HttpServletRequest request = mock(HttpServletRequest.class);
-//        HttpServletResponse response = mock(HttpServletResponse.class);
-//
-//        when(request.getParameter("username")).thenReturn("me");
-//        when(request.getParameter("password")).thenReturn("secret");
-//
-//        StringWriter stringWriter = new StringWriter();
-//        PrintWriter writer = new PrintWriter(stringWriter);
-//        when(response.getWriter()).thenReturn(writer);
-//
-//        verify(request, atLeast(1)).getParameter("username");
-//        writer.flush();
-//        Assert.assertTrue(stringWriter.toString().contains("My expected string"));
-//    }
 }
