488911312690ff18cfa6334f5b19cecf025c5040
João Lucas Pacheco A. Bastos <joaolucaspachecoab@gmail.com>
2018-12-11 11:32:06 -0200

    Encontrar email de expedidores adicionado

--- src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorNotifPortSemCiencia.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorNotifPortSemCiencia.java
@@ -7,7 +7,9 @@
 package br.ufg.inf.fabrica.conporta022018.controlador;
 
 import br.ufg.inf.fabrica.conporta022018.modelo.Designado;
+import br.ufg.inf.fabrica.conporta022018.modelo.Portaria;
 import br.ufg.inf.fabrica.conporta022018.persistencia.DesignadoDAO;
+import br.ufg.inf.fabrica.conporta022018.persistencia.PortariaDAO;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
@@ -21,6 +23,7 @@ public class ControladorNotifPortSemCiencia {
     public void verificarCiencia() throws ParseException {
 
         DesignadoDAO designadoDao = new DesignadoDAO();
+        PortariaDAO portariaDAO = new PortariaDAO();
 
         //Define o atraso limite apra a ciência
         Calendar cal = Calendar.getInstance();
@@ -35,7 +38,72 @@ public class ControladorNotifPortSemCiencia {
 
         List<String> emails = getEmailDesignados(designados);
 
-        this.enviarEmail(emails);
+        this.enviarEmailDesignados(emails);
+
+        List<Portaria> expedidores = portariaDAO.pesquisaExpedidorSemCiencia(formatter.parse(dataLimite));
+
+        emails = getEmailExpedidor(expedidores);
+
+        this.enviarEmailExpedidor(emails);
+
+    }
+
+    public void enviarEmailExpedidor(List<String> emails)
+    {
+        Properties props = new Properties();
+        /** Parâmetros de conexão com servidor Gmail */
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication()
+                    {
+                        return new PasswordAuthentication("conporta2018@gmail.com", "c0np0rt4");
+                    }
+                });
+
+        /** Ativa Debug para sessão */
+        session.setDebug(true);
+
+        String destinatarios = String.join(", ",emails);
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("conporta2018@gmail.com")); //Remetente
+
+            Address[] toUser = InternetAddress //Destinatário(s)
+                    .parse(destinatarios);
+
+            message.setRecipients(Message.RecipientType.TO, toUser);
+            message.setSubject("Enviando email com JavaMail");//Assunto
+            message.setText("Você possui designados sem ciência a plataforma");
+
+
+            /**Método para enviar a mensagem criada*/
+            Transport.send(message);
+
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private List<String> getEmailExpedidor(List<Portaria> expedidores) {
+        List<String> emails = null;
+        Iterator<Portaria> iterator = expedidores.iterator();
+
+        while (iterator.hasNext()){
+            emails.add(iterator.next().getExpedidor().getEmailPes());
+        }
+
+        return emails;
+
     }
 
     /**
@@ -53,9 +121,13 @@ public class ControladorNotifPortSemCiencia {
 
         List<String> emails = getEmailDesignados(designados);
 
-        this.enviarEmail(emails);
+        this.enviarEmailDesignados(emails);
+
+
+        List<Designado> expedidor = designadoDao.pesquisaDesignadosSemCiencia(formato.parse(dataLimite));
 
     }
+
 
 
     /**
@@ -81,7 +153,7 @@ public class ControladorNotifPortSemCiencia {
      * com o objetivo de facilitar a legibilidade do código
      * @param emails
      */
-    public void enviarEmail(List<String> emails)
+    public void enviarEmailDesignados(List<String> emails)
     {
         Properties props = new Properties();
         /** Parâmetros de conexão com servidor Gmail */
--- src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/PortariaDAO.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/PortariaDAO.java
@@ -1,7 +1,29 @@
 package br.ufg.inf.fabrica.conporta022018.persistencia;
 
+import br.ufg.inf.fabrica.conporta022018.modelo.Designado;
 import br.ufg.inf.fabrica.conporta022018.modelo.Portaria;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PortariaDAO extends GenericoDAO<Portaria> {
+
+    public List<Portaria> pesquisaExpedidorSemCiencia(Date dataLimite){
+
+        StringBuilder builder = new StringBuilder();
+        /*
+        * Ainda para fazer a query de pesquisa do banco de dados
+         */
+        builder.append("select p from Portaria d where p.dtCienciaDesig is null ");
+        builder.append(" and d.portaria.dtExped < :dtExped ");
+
+        Map<String, Object> parametros = new HashMap<>();
+        parametros.put("dtExped", dataLimite );
+
+        return this.pesquisarJPQLCustomizada(builder.toString(),parametros);
+
+    }
 
 }
