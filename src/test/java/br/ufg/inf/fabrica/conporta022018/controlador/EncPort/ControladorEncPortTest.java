fb72277b527806d550b900cac1b8a7c326fd64a9
Keslley L <keslley_14_20@hotmail.com>
2018-12-13 18:41:27 -0200

    Update ControladorEncPortTest.java

--- src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/EncPort/ControladorEncPortTest.java
+++ src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/EncPort/ControladorEncPortTest.java
@@ -9,13 +9,18 @@ package br.ufg.inf.fabrica.conporta022018.controlador.EncPort;
 import br.ufg.inf.fabrica.conporta022018.controlador.ControladorEncPort;
 import br.ufg.inf.fabrica.conporta022018.modelo.*;
 import br.ufg.inf.fabrica.conporta022018.persistencia.PessoaDAO;
+import br.ufg.inf.fabrica.conporta022018.persistencia.DesignadoDAO;
 import br.ufg.inf.fabrica.conporta022018.persistencia.PortariaDAO;
+import br.ufg.inf.fabrica.conporta022018.persistencia.RecebedoraDAO;
+import br.ufg.inf.fabrica.conporta022018.persistencia.GestaoDAO;
 import br.ufg.inf.fabrica.conporta022018.util.Extrator;
 import br.ufg.inf.fabrica.conporta022018.util.LerArquivo;
 import br.ufg.inf.fabrica.conporta022018.util.csv.ExtratorCSV;
 import org.junit.*;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 
@@ -25,14 +30,14 @@ public class ControladorEncPortTest {
     private static List<Designado> designados = new ArrayList();
     private static  List<Recebedora>  recebedoras = new ArrayList();
     private static ArrayList<Portaria> portarias = new ArrayList();
-
+    private static ArrayList<Gestao> gestaos = new ArrayList();
     /*
      * Preparação do ambiente para teste.
      * População do banco de Dados para atendam os pré-requisitos da seção de caso de uso.
      */
 
     @BeforeClass
-    public static void casoTestPepararCenario() throws IOException {
+    public static void casoTestPepararCenario() throws IOException, ParseException {
 
         String CAMINHO_CSV = "src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/EncPortDadosTest.csv";
         String REGRA = ";";
@@ -47,6 +52,9 @@ public class ControladorEncPortTest {
 
         PessoaDAO pessoaDAO = new PessoaDAO();
         PortariaDAO portariaDAO = new PortariaDAO();
+        DesignadoDAO designadoDAO = new DesignadoDAO();
+        RecebedoraDAO recebedoraDAO = new RecebedoraDAO();
+        GestaoDAO gestaoDAO = new GestaoDAO();
 
         // Objetos que são usados por externos
 
@@ -71,7 +79,7 @@ public class ControladorEncPortTest {
             }
 
 
-            if (linha.equals("UndAdmin")) {
+            if (linha.equals("recebedora")) {
                 tabelaAtual = linha;
                 index++;
                 continue;
@@ -137,17 +145,73 @@ public class ControladorEncPortTest {
                     } catch (Exception ex) {
                         portariaDAO.rollBackTransacao();
                     }
-                    break;
-                case "undAdm":
-                    extrator.setTexto(linha);
-                    dados = extrator.getResultado(REGRA);
+
                     break;
                 case "designado":
                     extrator.setTexto(linha);
                     dados = extrator.getResultado(REGRA);
+                    Designado designado = new Designado();
+                    designado.setDescrFuncDesig(dados[0]);
+                    designado.setDesignado(pessoa); //Essencial para o caso de uso ter os designados da portaria
+
+                    designadoDAO.abrirTransacao();
+
+                    try {
+                        Designado designaDoBanco = designadoDAO.salvar(designado);
+                        designados.add(designaDoBanco);
+                        designadoDAO.commitarTransacao();
+                    } catch (Exception ex) {
+                        portariaDAO.rollBackTransacao();
+                    }
+
                     break;
+
+                case "recebedora":
+                    extrator.setTexto(linha);
+
+                    Recebedora recebedora = new Recebedora();
+                    recebedora.setUnidadeRecebedora(undAdm); //Essencial para o caso de uso ter as UndRecebedora da portaria
+
+                    recebedoraDAO.abrirTransacao();
+
+                    try {
+                        Recebedora recebedoraDoBanco = recebedoraDAO.salvar(recebedora);
+                        recebedoras.add(recebedoraDoBanco);
+                        recebedoraDAO.commitarTransacao();
+                    } catch (Exception ex) {
+                        portariaDAO.rollBackTransacao();
+                    }
+
+                    break;
+
+                case "gestao":
+                    extrator.setTexto(linha);
+                    dados = extrator.getResultado(REGRA);
+
+                    String dataString = dados[0];
+                    SimpleDateFormat formataData=new SimpleDateFormat(dataString);
+                    Date date1=formataData.parse(dataString);
+
+                    Gestao gestao = new Gestao();
+                    gestao.setdtFim(date1);
+
+                    gestaoDAO.abrirTransacao();
+
+                    try {
+                        Gestao gestaoDoBanco = gestaoDAO.salvar(gestao);
+                        gestaos.add(gestaoDoBanco);
+                        gestaoDAO.commitarTransacao();
+                    } catch (Exception ex) {
+                        gestaoDAO.rollBackTransacao();
+                    }
+
+                    break;
+
             }
         }
     }
+
+    
+
 }
 
