27e5b947c25aa82575a6c9be4b7e8b66ef3cb064
Edionay Aguiar <edionay@gmail.com>
2018-12-14 20:34:20 -0200

    Importação de Discentes por CSV aprimorada.

--- src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorDisc.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorDisc.java
@@ -9,8 +9,6 @@ import br.ufg.inf.fabrica.conporta022018.modelo.*;
 import br.ufg.inf.fabrica.conporta022018.util.Extrator;
 import br.ufg.inf.fabrica.conporta022018.util.LerArquivo;
 import br.ufg.inf.fabrica.conporta022018.util.csv.ExtratorCSV;
-import com.google.gson.Gson;
-
 import br.ufg.inf.fabrica.conporta022018.persistencia.PessoaDAO;
 
 import java.io.IOException;
@@ -21,6 +19,7 @@ import java.util.*;
 public class ControladorDisc {
 
     private final String JPQL_BUSCAR_USUARIO_POR_CPF = "SELECT u FROM Pessoa u WHERE u.cpfPes = :CPF";
+    private final String JPQL_BUSCAR_USUARIO_POR_NOME = "SELECT u FROM Pessoa u WHERE u.nomePes = :NOME";
     private Map<String, Object> map = new HashMap<>();
     SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
     private Date defaultDate = new Date(01/01/2100);
@@ -37,16 +36,20 @@ public class ControladorDisc {
 
         PessoaDAO pessoaDAO = new PessoaDAO();
 
-        Pessoa pessoa = pessoaDAO.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_USUARIO_POR_CPF,map);
+        try {
+            Pessoa pessoa = pessoaDAO.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_USUARIO_POR_CPF,map);
 
-        pessoa.setDiscente(discente);
+            pessoa.setDiscente(discente);
 
-        pessoaDAO.abrirTransacao();
-        pessoaDAO.salvar(pessoa);
-        pessoaDAO.commitarTransacao();
+            pessoaDAO.abrirTransacao();
+            pessoaDAO.salvar(pessoa);
+            pessoaDAO.commitarTransacao();
 
-        return true;
-
+            return true;
+        } catch (Exception e) {
+            System.out.println("Exceção capturada: " + e);
+            return false;
+        }
     }
 
     public Boolean excluirDiscente(String cpfPessoa, Date dtFinalMatrCur) {
@@ -54,21 +57,30 @@ public class ControladorDisc {
         PessoaDAO pessoaDAO = new PessoaDAO();
         map.put("CPF", cpfPessoa);
 
-        List<Matricula> discente = new ArrayList<>();
+        List<Matricula> discente;
 
-        Pessoa pessoa = pessoaDAO.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_USUARIO_POR_CPF,map);
-        discente = pessoa.getDiscente();
+        try {
+            Pessoa pessoa = pessoaDAO.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_USUARIO_POR_CPF,map);
 
-        Matricula matricula = discente.get(discente.size() - 1);
-        matricula.setDtFimMatrCur(dtFinalMatrCur);
+            if (pessoa.getDiscente() == null) {
+                return null;
+            }
 
-        pessoaDAO.abrirTransacao();
-        pessoaDAO.salvar(pessoa);
-        pessoaDAO.commitarTransacao();
+            discente = pessoa.getDiscente();
 
-        return true;
+            Matricula matricula = discente.get(discente.size() - 1);
+            matricula.setDtFimMatrCur(dtFinalMatrCur);
 
-    };
+            pessoaDAO.abrirTransacao();
+            pessoaDAO.salvar(pessoa);
+            pessoaDAO.commitarTransacao();
+
+            return true;
+        } catch (Exception e) {
+            System.out.println("Exceção capturada: " + e);
+            return false;
+        }
+    }
 
     public Boolean importarDiscente(String caminhoDoArquivo) throws IOException, ParseException {
 
@@ -81,13 +93,11 @@ public class ControladorDisc {
         Date defaultDate = new Date(99/99/9999);
         String dados[];
         String linha;
-        Pessoa usuario;
-        Gestao gestao;
-        Lotacao lotacao;
+
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+
         Matricula matricula;
-        UndAdm undAdm;
         Curso curso;
-        PessoaDAO pessoaDAO = new PessoaDAO();
 
         dadosSoftware = lerArquivo.lerArquivo(CAMINHO_CSV);
 
@@ -96,7 +106,7 @@ public class ControladorDisc {
             linha = dadosSoftware.get(index);
 
             //Definir as tabelas que serão populadas no Banco de Dados.
-            if (linha.equals("pessoa")||linha.equals("perfil")) {
+            if (linha.equals("pessoa")) {
                 tabelaAtual = linha;
                 index++;
                 continue;
@@ -104,88 +114,19 @@ public class ControladorDisc {
 
             switch (tabelaAtual) {
                 case "pessoa":
-                    usuario = new Pessoa();
-                    gestao = new Gestao();
-                    lotacao = new Lotacao();
-                    matricula = new Matricula();
-                    curso = new Curso();
-                    undAdm = new UndAdm();
-                    SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-                    extrator.setTexto(linha);
-                    dados = extrator.getResultado(REGRA);
 
-                    usuario.setNomePes(dados[0]);
-                    usuario.setCpfPes(dados[1]);
-                    usuario.setEmailPes(dados[2]);
-                    usuario.setSenhaUsu(dados[3]);
-                    usuario.setEhUsuAtivo(Boolean.parseBoolean(dados[4]));
-                    if (dados[5].equals("null")) {
-                        usuario.setGestao(gestao);
-                    } else {
-                        gestao.setdtInicio(formato.parse(dados[5]));
-                        gestao.setdtFim(formato.parse(dados[6]));
-                        gestao.setDtIniSubChefe(formato.parse(dados[7]));
-                        gestao.setDtFimSubChefe(formato.parse(dados[8]));
 
-                        undAdm.setNomeUnd(dados[9]);
-                        undAdm.setSiglaUnAdm(dados[10]);
-                        String tipo = dados[11];
-                        if (tipo.equals("Curso"))
-                            undAdm.setTipoUnd(TipoUnd.CURSO);
-                        else if (tipo.equals("Unidade Acadêmica"))
-                            undAdm.setTipoUnd(TipoUnd.UNIDADE_ACADEMICA);
-                        else if (tipo.equals("Unidade Gestora"))
-                            undAdm.setTipoUnd(TipoUnd.UNIDADE_GESTORA);
-                        else if (tipo.equals("Conselho"))
-                            undAdm.setTipoUnd(TipoUnd.CONSELHO);
-                        else if (tipo.equals("Unidade Externa"))
-                            undAdm.setTipoUnd(TipoUnd.UNIDADE_EXTERNA);
-                        undAdm.setMinInat(Integer.parseInt(dados[12]));
-                        undAdm.setUltPort(dados[13]);
-                        undAdm.setAnoPort(Integer.parseInt(dados[14]));
-                        undAdm.setUltNumExped(Integer.parseInt(dados[15]));
-                        undAdm.setUltNumProp(Integer.parseInt(dados[16]));
-                        gestao.setUnAdm(undAdm);
-
-                        tipo = dados[17];
-                        if (tipo.equals("chefia"))
-                            gestao.setFuncao(Tipo.CHEFIA);
-                        else if (tipo.equals("coordenador adm"))
-                            gestao.setFuncao(Tipo.COORDENADOR_ADM);
-                        else if (tipo.equals("substituto"))
-                            gestao.setFuncao(Tipo.SUBSTITUTO);
-                        usuario.setGestao(gestao);
-                    }
-                    lotacao = new Lotacao();
-                    if (dados[18].equals("null")) {
-                        usuario.setServidor(lotacao);
-                    } else {
-                        lotacao.setDtIniLotServ(formato.parse(dados[18]));
-                        lotacao.setDtFimLotServ(formato.parse(dados[19]));
-                        lotacao.setDescrCargoServ(dados[20]);
-                        lotacao.setCargoServ(Cargo.valueOf(dados[21]));
-
-                        undAdm.setNomeUnd(dados[22]);
-                        undAdm.setSiglaUnAdm(dados[23]);
-                        undAdm.setTipoUnd(TipoUnd.UNIDADE_ACADEMICA);
-                        undAdm.setMinInat(Integer.parseInt(dados[25]));
-                        undAdm.setUltPort(dados[26]);
-                        undAdm.setAnoPort(Integer.parseInt(dados[27]));
-                        undAdm.setUltNumExped(Integer.parseInt(dados[28]));
-                        undAdm.setUltNumProp(Integer.parseInt(dados[29]));
-                        matricula.setMatrDiscCur(Integer.parseInt(dados[30]));
-                        matricula.setDtIniMatrCur(formato.parse(dados[31]));
-                        usuario.setDiscente(matricula);
-
-                        lotacao.setUndAdm(undAdm);
-                        usuario.setServidor(lotacao);
-                    }
-                    pessoaDAO.abrirTransacao();
                     try {
-                        pessoaDAO.salvar(usuario);
-                        pessoaDAO.commitarTransacao();
-                    } catch (Exception exc) {
-                        pessoaDAO.rollBackTransacao();
+                        dados = extrator.getResultado(REGRA);
+
+                        matricula = new Matricula();
+                        curso = new Curso();
+                        matricula.setCurso(curso);
+
+                        criarDiscente(dados[0], Integer.parseInt(dados[1]), formato.parse(dados[2]), curso);
+
+                    } catch (Exception e) {
+                        System.out.println("Exceção capturada: " + e);
                     }
                     break;
 
@@ -201,30 +142,50 @@ public class ControladorDisc {
         PessoaDAO pessoaDAO = new PessoaDAO();
         map.put("CPF", cpfPessoa);
 
-        Pessoa pessoa = pessoaDAO.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_USUARIO_POR_CPF,map);
 
-        List<Matricula> discente = new ArrayList<>();
+        try {
+            Pessoa pessoa = pessoaDAO.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_USUARIO_POR_CPF,map);
 
-        discente = pessoa.getDiscente();
+            List<Matricula> discente = new ArrayList<>();
 
-        Matricula matricula = discente.get(discente.size() - 1);
+            discente = pessoa.getDiscente();
 
-        matricula.setDtFimMatrCur(dtFinalMatrCur);
-        matricula.setDtFimMatrCur(dtFinalMatrCur);
+            Matricula matricula = discente.get(discente.size() - 1);
 
-        pessoaDAO.abrirTransacao();
-        pessoaDAO.salvar(pessoa);
-        pessoaDAO.commitarTransacao();
+            matricula.setDtFimMatrCur(dtFinalMatrCur);
+            matricula.setDtFimMatrCur(dtFinalMatrCur);
 
-        return true;
+            pessoaDAO.abrirTransacao();
+            pessoaDAO.salvar(pessoa);
+            pessoaDAO.commitarTransacao();
+
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Exceção capturada: " + e);
+            return false;
+
+        }
     }
 
-    public Pessoa buscarDiscente(String cpfPessoa) {
+    public Pessoa buscarDiscentePorCpf(String cpfPessoa) throws Exception{
 
         map.put("CPF", cpfPessoa);
         PessoaDAO pessoaDAO = new PessoaDAO();
-        Pessoa pessoa = pessoaDAO.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_USUARIO_POR_CPF,map);
 
-        return pessoa;
+        try {
+            Pessoa pessoa = pessoaDAO.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_USUARIO_POR_CPF, map);
+
+            if (pessoa.getDiscente() == null) {
+                return null;
+            }
+
+            return pessoa;
+
+        } catch (Exception e) {
+            System.out.println("Exceção capturada: " + e);
+            return null;
+        }
     }
+
 }
--- src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/manterDisc/ControladorManterDiscTest.java
+++ src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/manterDisc/ControladorManterDiscTest.java
@@ -8,6 +8,10 @@ package br.ufg.inf.fabrica.conporta022018.controlador.manterDisc;
 import br.ufg.inf.fabrica.conporta022018.controlador.ControladorDisc;
 import br.ufg.inf.fabrica.conporta022018.modelo.*;
 
+import br.ufg.inf.fabrica.conporta022018.persistencia.PessoaDAO;
+import br.ufg.inf.fabrica.conporta022018.util.Extrator;
+import br.ufg.inf.fabrica.conporta022018.util.LerArquivo;
+import br.ufg.inf.fabrica.conporta022018.util.csv.ExtratorCSV;
 import org.junit.*;
 import org.mockito.Mockito;
 
@@ -15,7 +19,9 @@ import java.io.IOException;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 /*
@@ -27,26 +33,142 @@ public class ControladorManterDiscTest extends Mockito {
     private static ControladorDisc controladorDisc = new ControladorDisc();
     SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
 
-
-
-
     @BeforeClass
     public static void casoTestImportacaoDiscente() throws IOException, ParseException {
 
-        // Verifica a importação e criação de Discentes a partir de um arquivo CSV
-        Assert.assertTrue(controladorDisc.importarDiscente("ControladorManterDisc.csv"));
+        String CAMINHO_CSV = "ControladorManterDisc.csv";
+        String REGRA = ",";
+        List<String> dadosSoftware = new ArrayList<>();
+        Extrator extrator = new ExtratorCSV();
+        LerArquivo lerArquivo = new LerArquivo();
+        String tabelaAtual = " ";
+        Date defaultDate = new Date(99/99/9999);
+        String dados[];
+        String linha;
+        Pessoa usuario;
+        Gestao gestao;
+        Lotacao lotacao;
+        Matricula matricula;
+        UndAdm undAdm;
+        Curso curso;
+        PessoaDAO pessoaDAO = new PessoaDAO();
 
+        dadosSoftware = lerArquivo.lerArquivo(CAMINHO_CSV);
+
+
+        for (int index = 0; index < dadosSoftware.size(); index++) {
+            linha = dadosSoftware.get(index);
+
+            //Definir as tabelas que serão populadas no Banco de Dados.
+            if (linha.equals("pessoa")||linha.equals("perfil")) {
+                tabelaAtual = linha;
+                index++;
+                continue;
+            }
+
+            switch (tabelaAtual) {
+                case "pessoa":
+                    usuario = new Pessoa();
+                    gestao = new Gestao();
+                    lotacao = new Lotacao();
+                    matricula = new Matricula();
+                    curso = new Curso();
+                    undAdm = new UndAdm();
+                    SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+                    extrator.setTexto(linha);
+                    dados = extrator.getResultado(REGRA);
+
+                    usuario.setNomePes(dados[0]);
+                    usuario.setCpfPes(dados[1]);
+                    usuario.setEmailPes(dados[2]);
+                    usuario.setSenhaUsu(dados[3]);
+                    usuario.setEhUsuAtivo(Boolean.parseBoolean(dados[4]));
+                    if (dados[5].equals("null")) {
+                        usuario.setGestao(gestao);
+                    } else {
+                        gestao.setdtInicio(formato.parse(dados[5]));
+                        gestao.setdtFim(formato.parse(dados[6]));
+                        gestao.setDtIniSubChefe(formato.parse(dados[7]));
+                        gestao.setDtFimSubChefe(formato.parse(dados[8]));
+
+                        undAdm.setNomeUnd(dados[9]);
+                        undAdm.setSiglaUnAdm(dados[10]);
+                        String tipo = dados[11];
+                        if (tipo.equals("Curso"))
+                            undAdm.setTipoUnd(TipoUnd.CURSO);
+                        else if (tipo.equals("Unidade Acadêmica"))
+                            undAdm.setTipoUnd(TipoUnd.UNIDADE_ACADEMICA);
+                        else if (tipo.equals("Unidade Gestora"))
+                            undAdm.setTipoUnd(TipoUnd.UNIDADE_GESTORA);
+                        else if (tipo.equals("Conselho"))
+                            undAdm.setTipoUnd(TipoUnd.CONSELHO);
+                        else if (tipo.equals("Unidade Externa"))
+                            undAdm.setTipoUnd(TipoUnd.UNIDADE_EXTERNA);
+                        undAdm.setMinInat(Integer.parseInt(dados[12]));
+                        undAdm.setUltPort(dados[13]);
+                        undAdm.setAnoPort(Integer.parseInt(dados[14]));
+                        undAdm.setUltNumExped(Integer.parseInt(dados[15]));
+                        undAdm.setUltNumProp(Integer.parseInt(dados[16]));
+                        gestao.setUnAdm(undAdm);
+
+                        tipo = dados[17];
+                        if (tipo.equals("chefia"))
+                            gestao.setFuncao(Tipo.CHEFIA);
+                        else if (tipo.equals("coordenador adm"))
+                            gestao.setFuncao(Tipo.COORDENADOR_ADM);
+                        else if (tipo.equals("substituto"))
+                            gestao.setFuncao(Tipo.SUBSTITUTO);
+                        usuario.setGestao(gestao);
+                    }
+                    lotacao = new Lotacao();
+                    if (dados[18].equals("null")) {
+                        usuario.setServidor(lotacao);
+                    } else {
+                        lotacao.setDtIniLotServ(formato.parse(dados[18]));
+                        lotacao.setDtFimLotServ(formato.parse(dados[19]));
+                        lotacao.setDescrCargoServ(dados[20]);
+                        lotacao.setCargoServ(Cargo.valueOf(dados[21]));
+
+                        undAdm.setNomeUnd(dados[22]);
+                        undAdm.setSiglaUnAdm(dados[23]);
+                        undAdm.setTipoUnd(TipoUnd.UNIDADE_ACADEMICA);
+                        undAdm.setMinInat(Integer.parseInt(dados[25]));
+                        undAdm.setUltPort(dados[26]);
+                        undAdm.setAnoPort(Integer.parseInt(dados[27]));
+                        undAdm.setUltNumExped(Integer.parseInt(dados[28]));
+                        undAdm.setUltNumProp(Integer.parseInt(dados[29]));
+                        matricula.setMatrDiscCur(Integer.parseInt(dados[30]));
+                        matricula.setDtIniMatrCur(formato.parse(dados[31]));
+                        usuario.setDiscente(matricula);
+
+                        lotacao.setUndAdm(undAdm);
+                        usuario.setServidor(lotacao);
+                    }
+                    pessoaDAO.abrirTransacao();
+                    try {
+                        pessoaDAO.salvar(usuario);
+                        pessoaDAO.commitarTransacao();
+                    } catch (Exception exc) {
+                        pessoaDAO.rollBackTransacao();
+                    }
+                    break;
+
+            }
+        }
+
+        // Verifica a importação e criação de Discentes a partir de um arquivo CSV
+//        Assert.assertTrue(controladorDisc.importarDiscente("ControladorManterDisc.csv"));
     }
 
-
     @Test
-    public void casoTestBuscaDeDiscente() throws IOException {
+    public void casoTestBuscaDeDiscente() throws Exception {
 
         // Verifica a consistência de um dos dados criados acima e testa a Busca de Discente
-        Pessoa pessoa2 = controladorDisc.buscarDiscente("123.123.123-12");
+        Pessoa pessoa2 = controladorDisc.buscarDiscentePorCpf("123.123.123-12");
         Assert.assertEquals("123.123.123-12", pessoa2.getCpfPes());
 
     }
+
 
     @Test
     public void casoTestCriacaoDeDiscente() throws IOException, ParseException {
