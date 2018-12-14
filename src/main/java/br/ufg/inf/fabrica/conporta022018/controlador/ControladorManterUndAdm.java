0165a60b2a980d7fa3551d52cd2982ac87adbbbb
Antonio Silva <antoni-s@outlook.com>
2018-12-14 17:25:35 -0200

    Alteração de Exception.

--- src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorManterUndAdm.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorManterUndAdm.java
@@ -7,6 +7,7 @@ package br.ufg.inf.fabrica.conporta022018.controlador;
 
 import br.ufg.inf.fabrica.conporta022018.modelo.UndAdm;
 import br.ufg.inf.fabrica.conporta022018.persistencia.UndAdmDAO;
+import javax.persistence.NoResultException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,10 +28,9 @@ public class ControladorManterUndAdm {
     }
 
     public void editarTimeOut(int minInat, String sigla) throws Exception {
-        this.map.put("sigla", sigla);
-        this.undAdm = dao.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_UNIDADE, map);
-
-        if (!this.undAdm.equals(null)) {
+        try {
+            this.map.put("sigla", sigla);
+            this.undAdm = dao.pesquisarUmJPQLCustomizada(JPQL_BUSCAR_UNIDADE, map);
 
             if (minInat <= this.TEMPO_MAXIMO && minInat >= this.TEMPO_MINIMO) {
                 undAdm.setMinInat(minInat);
@@ -45,7 +45,7 @@ public class ControladorManterUndAdm {
                 throw new Exception("Tempo Inválido! Por favor, Digite um Tempo para inatividade entre 15 e 60 " +
                         "segundos, inclusive-os.");
             }
-        } else {
+        } catch (NoResultException e) {
             throw new Exception("A Unidade informada não foi localizada.");
         }
     }
