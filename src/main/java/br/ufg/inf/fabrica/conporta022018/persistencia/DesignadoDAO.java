944385b250291e355c9256ef05a6a226b464aed7
Antonio Silva <antoni-s@outlook.com>
2018-12-21 20:50:03 -0200

    Update DesignadoDAO.java

--- src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/DesignadoDAO.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/DesignadoDAO.java
@@ -2,10 +2,6 @@ package br.ufg.inf.fabrica.conporta022018.persistencia;
 
 import br.ufg.inf.fabrica.conporta022018.modelo.Designado;
 
-<<<<<<< HEAD
 public class DesignadoDAO extends GenericoDAO<Designado>{
-=======
-public class DesignadoDAO extends GenericoDAO<Designado> {
->>>>>>> ediTemTim
 
 }
